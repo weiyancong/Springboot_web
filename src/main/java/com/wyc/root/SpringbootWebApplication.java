1f4abcb5a036eff5ed835778c33ce0e88c8d0d5e
weiyancong <2919296013@qq.com>
2021-07-14 22:50:05 +0800

    idea第二次提交本地库

--- src/main/java/com/wyc/root/SpringbootWebApplication.java
+++ src/main/java/com/wyc/root/SpringbootWebApplication.java
@@ -10,6 +10,7 @@ public class SpringbootWebApplication
     public static void main(String[] args)
     {
         SpringApplication.run(SpringbootWebApplication.class, args);
+        System.out.println("第二次提交！");
     }
 
 }
